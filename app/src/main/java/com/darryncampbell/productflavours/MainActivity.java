8d4795a2e1f020cdc28e5f91db0b27a8951d3a90
darryncampbell <jrq768@zebra.com>
2017-02-06 14:32:31 +0000

    Implementing EMDKListener

--- app/src/main/java/com/darryncampbell/productflavours/MainActivity.java
+++ app/src/main/java/com/darryncampbell/productflavours/MainActivity.java
@@ -2,12 +2,26 @@ package com.darryncampbell.productflavours;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.symbol.emdk.EMDKManager;
+import com.symbol.emdk.EMDKResults;
+import com.symbol.emdk.ProfileManager;
+import com.symbol.emdk.EMDKManager.EMDKListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EMDKListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onOpened(EMDKManager emdkManager) {
+
+    }
+
+    @Override
+    public void onClosed() {
+
     }
 }
